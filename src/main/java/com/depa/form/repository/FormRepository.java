7198b8a3c9b87ecbe4271d58c4ceab53ad8475e7
Imagine Rabbits <keerati.jear@gmail.com>
2020-04-05 19:57:38 +0700

    [ADD] add repository annotations

--- src/main/java/com/depa/form/repository/FormRepository.java
+++ src/main/java/com/depa/form/repository/FormRepository.java
@@ -2,7 +2,9 @@ package com.depa.form.repository;
 
 import com.depa.form.model.form.Form;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FormRepository extends JpaRepository<Form, String> {
     Form save(Form form);
 }
