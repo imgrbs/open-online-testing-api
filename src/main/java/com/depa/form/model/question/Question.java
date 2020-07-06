9c10515ad6ba86d68c9e9dc41b08e53699da2d7f
bazsup <supawit.ruen@mail.kmutt.ac.th>
2020-07-06 19:20:57 +0700

    [UPDATE] assign id attribute in Question model

--- src/main/java/com/depa/form/model/question/Question.java
+++ src/main/java/com/depa/form/model/question/Question.java
@@ -3,6 +3,7 @@ package com.depa.form.model.question;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 @Data
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "type")
@@ -11,6 +12,8 @@ import lombok.Data;
     @JsonSubTypes.Type(value = ChoiceQuestion.class, name = "choice"),
 })
 public abstract class Question {
+    @Id
+    private String id;
     protected QuestionType type;
     protected String questionText;
 }
