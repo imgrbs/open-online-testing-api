7a8406148cc7f1d41b3551c4c3b89303a06e6b05
Imagine Rabbits <keerati.jear@gmail.com>
2020-07-12 19:40:08 +0700

    [UPDATE] change id type of question

--- src/main/java/com/depa/form/model/question/Question.java
+++ src/main/java/com/depa/form/model/question/Question.java
@@ -3,18 +3,21 @@ package com.depa.form.model.question;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
 
+@Document("questions")
 @Getter
 @Setter
 @AllArgsConstructor
 public abstract class Question {
     
     @Id
-    private long id;
+    private ObjectId id;
 
     private String name;
     
