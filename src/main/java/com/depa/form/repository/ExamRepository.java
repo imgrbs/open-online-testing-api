cdff718c1306faf87a32e9987bb23d5dbdbf0f79
Imagine Rabbits <keerati.jear@gmail.com>
2020-07-12 21:06:20 +0700

    [REMOVE] delete comment

--- src/main/java/com/depa/form/repository/ExamRepository.java
+++ src/main/java/com/depa/form/repository/ExamRepository.java
@@ -3,9 +3,5 @@ package com.depa.form.repository;
 import com.depa.form.model.exam.Exam;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-/**
- *
- * @author Test
- */
 public interface ExamRepository extends MongoRepository<Exam, String>{
 }
