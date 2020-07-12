dac5d8ac32016fbe33fc1c8ad8e1c5bc1f404406
Imagine Rabbits <keerati.jear@gmail.com>
2020-07-12 21:05:59 +0700

    [UPDATE] change choice's variable name

--- src/main/java/com/depa/form/model/question/ObjectiveQuestion.java
+++ src/main/java/com/depa/form/model/question/ObjectiveQuestion.java
@@ -1,34 +1,33 @@
 package com.depa.form.model.question;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ObjectiveQuestion extends Question {
 
-    private List<ChoiceList> choices;
+    private List<Choice> choices;
 
     public ObjectiveQuestion(QuestionType questionType) {
         super(questionType);
     }
 
-    public static ObjectiveQuestion create(List<Attribute> attributes, List<ChoiceList> choices) {
+    public static ObjectiveQuestion create(List<Attribute> attributes, List<Choice> choices) {
         ObjectiveQuestion field = new ObjectiveQuestion(QuestionType.OBJECTIVE);
         field.setAttributes(attributes);
         field.setChoices(choices);
         return field;
     }
 
-    public static ObjectiveQuestion create(String name, List<ChoiceList> choices, List<Attribute> attributes) {
+    public static ObjectiveQuestion create(String name, List<Choice> choices, List<Attribute> attributes) {
         ObjectiveQuestion question = create(attributes, choices);
         question.setName(name);
         return question;
     }
 
-    public List<ChoiceList> getChoices() {
+    public List<Choice> getChoices() {
         return choices;
     }
 
-    public void setChoices(List<ChoiceList> choices) {
+    public void setChoices(List<Choice> choices) {
         this.choices = choices;
     }
 
