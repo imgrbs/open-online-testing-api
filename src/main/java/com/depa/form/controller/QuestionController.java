2cda5968393b8aaad7fc8ec1d38112e942299de1
Imagine Rabbits <keerati.jear@gmail.com>
2020-07-12 19:35:12 +0700

    [ADD] add get mapping /questions

--- src/main/java/com/depa/form/controller/QuestionController.java
+++ src/main/java/com/depa/form/controller/QuestionController.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,6 +18,7 @@ public class QuestionController {
     @Autowired
     private QuestionService questionService;
 
+    @GetMapping("/questions")
     public ResponseEntity<List<QuestionDTO>> getQuestions() {
         List<QuestionDTO> questions = questionService.getQuestions();
         return new ResponseEntity<>(questions, HttpStatus.OK);
