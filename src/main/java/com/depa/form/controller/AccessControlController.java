7cc6fad9c93aa8c77772e5798db24953de12ad26
NaomiLin <wdrdres3qew5ts21@gmail.com>
2020-07-12 22:28:39 +0700

    [ADD] Template resource มี permission ไว้เช็ค

--- src/main/java/com/depa/form/controller/AccessControlController.java
+++ src/main/java/com/depa/form/controller/AccessControlController.java
@@ -6,6 +6,8 @@
 package com.depa.form.controller;
 
 import com.depa.form.dto.UserViewExamDTO;
+import com.depa.form.model.resource.Exam;
+import com.depa.form.model.resource.Problem;
 import java.util.HashMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +25,18 @@ public class AccessControlController {
     public Object isHadPermission(@RequestBody UserViewExamDTO userViewExam) {
         System.out.println(userViewExam);
         HashMap<String, Object> response = new HashMap();
+        Problem existingProblem1 = new Problem();
+        existingProblem1.setProblemId("problem-1-dmsjf9q2");
+        existingProblem1.setProblemName("Which are distribute tracing products (Select all correct answer from choice)");
+        
+        Problem existingProblem2 = new Problem();
+        existingProblem2.setProblemId("problem-2-dmsjf9q2");
+        existingProblem1.setProblemName("Describe characacteristic of good practice in keeping logs (In you opinion and the reason and theory behind your ideas)");
+        
+        Exam existingExam = new Exam();
+        existingExam.setExamId("existing_543-to-d@y");
+        existingExam.setExamName("Cloud Native: Distribute Tracing");
+
         response.put("isHadPermission", false);
         return response;
     }
