3b23833eb01e9aa57ea27339d7efaa29f9fc5327
Imagine Rabbits <keerati.jear@gmail.com>
2020-07-12 20:13:07 +0700

    [UPDATE] change question type to objective and subjective in enum

--- src/main/java/com/depa/form/model/question/QuestionType.java
+++ src/main/java/com/depa/form/model/question/QuestionType.java
@@ -3,17 +3,6 @@ package com.depa.form.model.question;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public enum QuestionType {
-    @JsonProperty("input")
-    INPUT,
-    @JsonProperty("textfield")
-    TEXTFIELD,
-    @JsonProperty(value = "textarea")
-    TEXTAREA,
-    @JsonProperty("number")
-    NUMBER,
-    @JsonProperty("select")
-    CHECKBOX,
-    @JsonProperty(value = "radio")
-    RADIO,
- 
+    SUBJECTIVE,
+    OBJECTIVE,
 }
