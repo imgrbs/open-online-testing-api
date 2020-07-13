e59d8551a71db03ad969054a554bd300c56cfd26
Imagine Rabbits <keerati.jear@gmail.com>
2020-07-14 02:52:58 +0700

    [ADD] add exam service with create, get all, get by object id

---
+++ src/main/java/com/depa/exam/service/ExamService.java
@@ -0,0 +1,20 @@
+package com.depa.exam.service;
+
+import com.depa.exam.dto.ExamDTO;
+import com.depa.exam.model.exam.Exam;
+import com.depa.exam.repository.ExamRepository;
+import org.bson.types.ObjectId;
+
+import java.util.List;
+
+public interface ExamService {
+    void setExamRepository(ExamRepository mockExamRepository);
+
+    ExamDTO createExam(ExamDTO exam);
+
+    List<ExamDTO> getExams();
+
+    ExamDTO getExamById(ObjectId id);
+
+    ExamDTO toExamDTO(Exam exam);
+}
---
+++ src/main/java/com/depa/exam/service/internal/ExamServiceImpl.java
@@ -0,0 +1,47 @@
+package com.depa.exam.service.internal;
+
+import com.depa.exam.dto.ExamDTO;
+import com.depa.exam.dto.impl.ExamDTOImpl;
+import com.depa.exam.model.exam.Exam;
+import com.depa.exam.repository.ExamRepository;
+import com.depa.exam.service.ExamService;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Service
+public class ExamServiceImpl implements ExamService {
+
+    @Autowired
+    private ExamRepository examRepository;
+
+    @Override
+    public ExamDTO createExam(ExamDTO examDTO) {
+        Exam exam = examRepository.save(examDTO.toExam());
+        return toExamDTO(exam);
+    }
+
+    @Override
+    public List<ExamDTO> getExams() {
+        List<Exam> queryExams = this.examRepository.findAll();
+
+        ArrayList<ExamDTO> exams = new ArrayList<>();
+        queryExams.forEach(exam -> exams.add(toExamDTO(exam)));
+        return exams;
+    }
+
+    @Override
+    public ExamDTO getExamById(ObjectId id) {
+        return toExamDTO(examRepository.findById(id).get());
+    }
+
+    @Override
+    public ExamDTO toExamDTO(Exam exam) {
+        return new ExamDTOImpl(exam);
+    }
+}
