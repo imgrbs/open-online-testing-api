a6ae5690fcb306fa58f4f51164f1d9de78bc71dd
Imagine Rabbits <keerati.jear@gmail.com>
2020-07-14 02:52:39 +0700

    [ADD] add exam dto

---
+++ src/main/java/com/depa/exam/dto/ExamDTO.java
@@ -0,0 +1,23 @@
+package com.depa.exam.dto;
+
+import com.depa.exam.dto.impl.QuestionDTOImpl;
+import com.depa.exam.model.exam.Exam;
+
+import java.util.List;
+
+public interface ExamDTO {
+
+    Exam toExam();
+
+    String getName();
+
+    void setName(String name);
+
+    String getDescription();
+
+    void setDescription(String description);
+
+    List<QuestionDTOImpl> getQuestions();
+
+    void setQuestions(List<QuestionDTOImpl> questions);
+}
