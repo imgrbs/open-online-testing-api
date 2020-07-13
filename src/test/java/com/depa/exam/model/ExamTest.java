14fccac5a3affe55819e247bf53d60ccb6b2f0fd
Imagine Rabbits <keerati.jear@gmail.com>
2020-07-14 02:52:30 +0700

    [ADD] add exam test

---
+++ src/test/java/com/depa/exam/model/ExamTest.java
@@ -0,0 +1,57 @@
+package com.depa.exam.model;
+
+import com.depa.exam.dto.QuestionDTO;
+import com.depa.exam.dto.impl.ExamDTOImpl;
+import com.depa.exam.dto.impl.QuestionDTOImpl;
+import com.depa.exam.dto.ExamDTO;
+import com.depa.exam.model.exam.Exam;
+import com.depa.exam.model.question.QuestionType;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ExamTest {
+
+    @Test
+    void testCreateExam() {
+        Exam underTest = new Exam();
+
+        Assert.assertThat(underTest.getName(), CoreMatchers.equalTo(null));
+        Assert.assertThat(underTest.getDescription(), CoreMatchers.equalTo(null));
+        Assert.assertThat(underTest.getQuestions(), CoreMatchers.equalTo(null));
+    }
+
+    @Test
+    public void testCreateExamWithExamDTO() {
+        List<QuestionDTOImpl> questions = createQuestions();
+
+        ExamDTO examDTO = new ExamDTOImpl();
+        examDTO.setQuestions(questions);
+
+        Exam underTest = new Exam(examDTO);
+
+        Assert.assertThat(underTest.getName(), CoreMatchers.equalTo(null));
+        Assert.assertThat(underTest.getDescription(), CoreMatchers.equalTo(null));
+        Assert.assertThat(underTest.getQuestions().size(), CoreMatchers.equalTo(2));
+        Assert.assertThat(underTest.getQuestions().get(0).getType(), CoreMatchers.equalTo(QuestionType.SUBJECTIVE));
+        Assert.assertThat(underTest.getQuestions().get(1).getType(), CoreMatchers.equalTo(QuestionType.OBJECTIVE));
+    }
+
+    private List<QuestionDTOImpl> createQuestions() {
+        List<QuestionDTOImpl> questions = new ArrayList<>();
+        QuestionDTOImpl question1 = createQuestion(QuestionType.SUBJECTIVE);
+        QuestionDTOImpl question2 = createQuestion(QuestionType.OBJECTIVE);
+        questions.add(question1);
+        questions.add(question2);
+        return questions;
+    }
+
+    private QuestionDTOImpl createQuestion(QuestionType type) {
+        QuestionDTOImpl question = new QuestionDTOImpl();
+        question.setType(type);
+        return question;
+    }
+}
