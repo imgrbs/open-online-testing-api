b75bd59dea14b6c0f41e30f13ae4aad110f95b93
bazsup <supawit.ruen@mail.kmutt.ac.th>
2020-07-18 12:31:55 +0700

    [UPDATE] allow cors for question controller

--- src/main/java/com/depa/exam/controller/QuestionController.java
+++ src/main/java/com/depa/exam/controller/QuestionController.java
@@ -7,15 +7,13 @@ import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Setter
 @RestController
+@CrossOrigin(origins = "*")
 public class QuestionController {
     @Autowired
     private QuestionService questionService;
