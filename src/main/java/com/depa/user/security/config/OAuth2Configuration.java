0ee4c4e66944497ae4b4092e3d656b62abde53b2
Imagine Rabbits <keerati.jear@gmail.com>
2020-07-21 01:12:40 +0700

    [ADD] add custom configuration in oauth file

--- src/main/java/com/depa/user/security/config/OAuth2Configuration.java
+++ src/main/java/com/depa/user/security/config/OAuth2Configuration.java
@@ -2,6 +2,8 @@ package com.depa.user.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
@@ -10,7 +12,24 @@ import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
 
 @Configuration
-public class OAuth2Configuration {
+public class OAuth2Configuration extends WebSecurityConfigurerAdapter {
+
+    private static final String[] WHITELIST = {
+            "/exams/**",
+            "/questions/**",
+    };
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.httpBasic().disable();
+        httpSecurity
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(WHITELIST).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login();
+    }
 
     @Bean
     public ClientRegistrationRepository clientRegistrationRepository() {
@@ -25,7 +44,7 @@ public class OAuth2Configuration {
         return ClientRegistration.withRegistrationId("depa")
                 .clientId("google-client-id")
                 .clientSecret("google-client-secret")
-                .clientAuthenticationMethod(ClientAuthenticationMethod.BASIC)
+                .clientAuthenticationMethod(ClientAuthenticationMethod.POST)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .redirectUriTemplate("{baseUrl}/login/oauth2/code/{registrationId}")
                 .scope("openid", "profile", "email", "address", "phone")
