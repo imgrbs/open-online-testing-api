080b30b9bf876467af329f4c99a13de8cc4a77c5
Imagine Rabbits <keerati.jear@gmail.com>
2020-07-20 22:44:20 +0700

    [ADD] add user adapter

---
+++ src/main/java/com/depa/user/model/user/impl/FacebookUserAdapter.java
@@ -0,0 +1,15 @@
+package com.depa.user.model.user.impl;
+
+import com.depa.user.model.user.User;
+
+public class FacebookUserAdapter implements User {
+    @Override
+    public String getEmail() {
+        return null;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return null;
+    }
+}
---
+++ src/main/java/com/depa/user/model/user/impl/GoogleUserAdapter.java
@@ -0,0 +1,15 @@
+package com.depa.user.model.user.impl;
+
+import com.depa.user.model.user.User;
+
+public class GoogleUserAdapter implements User {
+    @Override
+    public String getEmail() {
+        return null;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return null;
+    }
+}
