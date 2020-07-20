ea020aaece181199e9802c8698d8ef5a592a5b17
Imagine Rabbits <keerati.jear@gmail.com>
2020-07-20 21:22:59 +0700

    [REMOVE] clear unused method

--- src/main/java/com/depa/user/controller/AuthorizeController.java
+++ src/main/java/com/depa/user/controller/AuthorizeController.java
@@ -3,7 +3,6 @@ package com.depa.user.controller;
 import com.depa.user.service.internal.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,16 +11,6 @@ public class AuthorizeController {
 
     @Autowired
     private UserServiceImpl userService;
-
-    @GetMapping("/")
-    public String helloWorld(){
-        return "Hello World";
-    }
-
-    @GetMapping("/restricted")
-    public String restricted(){
-        return "restricted ja";
-    }
 
     @RequestMapping(value = "/user")
     public Object user(Authentication authentication){
