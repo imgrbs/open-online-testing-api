0050589a4256141099fc92b3fbc0b8a00f1dd875
Imagine Rabbits <keerati.jear@gmail.com>
2020-07-21 00:22:38 +0700

    [UPDATE] refactor loc

--- src/main/java/com/depa/user/security/config/CustomAuthConfiguration.java
+++ src/main/java/com/depa/user/security/config/CustomAuthConfiguration.java
@@ -1,23 +1,18 @@
 package com.depa.user.security.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CustomAuthConfiguration extends WebSecurityConfigurerAdapter {
 
     private static final String[] WHITELIST = {
-        "/exams/**",
-        "/questions/**",
+            "/exams/**",
+            "/questions/**",
     };
 
     @Override
