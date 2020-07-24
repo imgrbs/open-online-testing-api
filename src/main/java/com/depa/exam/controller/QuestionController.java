3c1984e52158d616615531c1697c1fc4932ee172
NaomiLin <wdrdres3qew5ts21@gmail.com>
2020-07-24 16:16:40 +0700

    [FIX] อนุญาต CrossOrigin บน QuestionController

--- src/main/java/com/depa/exam/controller/QuestionController.java
+++ src/main/java/com/depa/exam/controller/QuestionController.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import java.util.List;
 
 @Setter
 @RestController
+@CrossOrigin(origins = "*")
 public class QuestionController {
     @Autowired
     private QuestionService questionService;
